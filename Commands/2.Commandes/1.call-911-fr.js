c9b76800787971ab81fd7408649b4f8222c18071
TutoTek <74512450+TutoTek@users.noreply.github.com>
2021-07-26 14:15:02 +0000

    Update 1.call-911-fr.js

--- Commands/2.Commandes/1.call-911-fr.js
+++ Commands/2.Commandes/1.call-911-fr.js
@@ -2,22 +2,16 @@ const { COMMANDS } = require("../../Utilitaire/Constants");
 const { MessageEmbed } = require("discord.js");
 const fs = require("fs");
 const Telephone = Object.values(JSON.parse(fs.readFileSync("./call-911-fr.json")));
-
 module.exports.run = async(client, message, args, Prefix) => {
   let channel = message.channel;
   let user = message.author;
-
   message.delete();
-
   const category = message.guild.channels.cache.get("838041363143458816") ? message.guild.channels.cache.get("838041363143458816") : false
-
   if(!category) return channel.send("La catégorie n'est pas valide. Veyez modifier l'id dans le code.")
-
   await message.guild.channels.create(`911 call ${user.tag}`, {
     topic: `911 call ${user.tag} (${user.id})`,
     parent: category
   }).then(chnl => channel = chnl);
-
   channel.send(
     new MessageEmbed()
     .setColor("#FF0000")
@@ -25,7 +19,6 @@ module.exports.run = async(client, message, args, Prefix) => {
     .setDescription("911, quelle est votre urgence ?")
     .setFooter("(c)call 911 | 2021.")
   );
-
   function fail(reason) {
     channel.send(
       new MessageEmbed()
@@ -50,11 +43,8 @@ module.exports.run = async(client, message, args, Prefix) => {
     }, 15000);
     return;
   }
-
   let intervention = Object.values(Telephone[Math.floor(Math.random() * Telephone.length)]);
-
   for(let i = 0; i < intervention.length; i++) {
-
     await channel.send(
       new MessageEmbed()
       .setTitle(intervention[i].titre)
@@ -76,26 +66,29 @@ module.exports.run = async(client, message, args, Prefix) => {
         }, 5000);
       }
     });
-
     let faire;
-
     let filter = (message) => { return message.author.id == user.id; }
     await channel.awaitMessages(filter, { max: 1, time: 60000, errors: ['time'] }).then(async collected => {
       let newMessage = await collected.first();
       if(i == intervention.length-1) {
         faire = "win";
-      } else if(newMessage.content != `${intervention[i].reponse}`) {
+      } else if(newMessage.content !== `${intervention[i].reponse}`) {
         faire = ["lost", intervention[i].reason];
+      }else {
+        faire = "win";
       }
     }).catch((err) => {
       faire = ["lost", "Le temps est écoulé..."];
     });
 
     if(faire == "win") {
-      return success();
-    } else if(faire[0] = "lost") {
-      return fail(faire[1]);
-    }
+      if(i==intervention.length-1){
+        return success();
+      }
+
+    } else if(faire = "lost") {
+      return fail(faire);
+    }else console.log("suite")
   }
 }
 
